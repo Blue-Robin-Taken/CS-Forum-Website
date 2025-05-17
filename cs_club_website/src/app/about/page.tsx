898984002b9efa6de746e2716de234395f4e7919
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-05-17 12:01:19 -0500

    Never mind, the preset globals are actually kinda useful!!

--- cs_club_website/src/app/about/page.tsx
+++ cs_club_website/src/app/about/page.tsx
@@ -1,7 +1,18 @@
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="flex flex-col">
+            <h1 className="text-7xl m-2 p-2 align-middle justify-center text-center">
+                About
+            </h1>
+            <div className="">
+                <h2 className="text-3xl m-5 p-2 align-middle justify-center text-center">
+                    Who are we?
+                </h2>
+                <p className="text-xl ">
+                    We're the Computer Science Club/Forum at Allen High School.
+                    We're more than a decade old and have many sub clubs.
+                </p>
+            </div>
         </div>
     );
 }
