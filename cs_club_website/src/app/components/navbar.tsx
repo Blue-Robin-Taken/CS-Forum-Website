86c608902265b7727a33f315ca82ef2b0da88831
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-05-17 11:41:06 -0500

    removed everything in globals because Advaiya hates globals

--- cs_club_website/src/app/components/navbar.tsx
+++ cs_club_website/src/app/components/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 export default function NavBar() {
     return (
-        <div>
+        <div className="flex flex-row text-3xl space-x-5 p-2 m-2">
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
         </div>
