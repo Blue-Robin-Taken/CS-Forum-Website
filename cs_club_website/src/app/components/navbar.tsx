a8d89d3dc11da7215aac5bfa580f1e443546d73c
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-05-17 11:37:45 -0500

    about page and main page with navbar (not formatted yet)

---
+++ cs_club_website/src/app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function NavBar() {
+    return (
+        <div>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+        </div>
+    );
+}
