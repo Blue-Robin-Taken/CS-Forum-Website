329409cb7f99711f9473809cf77d55fd64dc9fa3
Blue-Robin-Taken <julianjohnt@gmail.com>
2025-05-17 18:24:11 -0500

    fixed some typescript issues w eslint

--- cs_club_website/src/app/components/background.tsx
+++ cs_club_website/src/app/components/background.tsx
@@ -4,11 +4,12 @@ import { useEffect, useMemo, useState } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
 import { loadFull } from 'tsparticles';
 import { none } from '@tsparticles/engine';
+import { IOptions } from '@tsparticles/engine';
 
 // https://stackoverflow.com/a/77396820/15982771 & https://github.com/tsparticles/react/#readme
 export default function Background() {
     const [init, setInit] = useState(false);
-    const [options, setOptions] = useState(null);
+    const [options, setOptions] = useState<IOptions | null>(null);
     useEffect(() => {
         initParticlesEngine(async (engine) => {
             await loadFull(engine);
@@ -26,7 +27,7 @@ export default function Background() {
         console.log(container);
     };
 
-    if (init) {
+    if (init && options) {
         return (
             <Particles
                 id="tsparticles"
